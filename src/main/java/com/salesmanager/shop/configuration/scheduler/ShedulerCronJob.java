7b3db5cb69b769ed9ddc56a8752b7fb6b6f17fc4
Marcos <jmarcosjm@gmail.com>
2017-08-28 19:26:06 +0100

    changes to adapt admin to ProductPrice model

--- src/main/java/com/salesmanager/shop/configuration/scheduler/ShedulerCronJob.java
+++ src/main/java/com/salesmanager/shop/configuration/scheduler/ShedulerCronJob.java
@@ -2,18 +2,22 @@ package com.salesmanager.shop.configuration.scheduler;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import com.salesmanager.business.marketing.services.EmailMarketingService;
+import com.salesmanager.business.marketing.services.PromotionService;
 import com.salesmanager.business.scheduler.services.JobResultService;
 import com.salesmanager.business.scheduler.services.SchedulerService;
 import com.salesmanager.core.business.exception.ServiceException;
 import com.salesmanager.marketing.model.EmailMarketing;
+import com.salesmanager.marketing.model.Promotion;
 import com.salesmanager.scheduler.model.JobResult;
 import com.salesmanager.scheduler.model.Scheduler;
+import com.salesmanager.shop.configuration.marketing.PromotionType;
 
 @Configuration
 public class ShedulerCronJob {
@@ -23,6 +27,9 @@ public class ShedulerCronJob {
 	private JobResultService jobResultService;
 	@Inject
 	private EmailMarketingService emailMarketingService;
+
+	@Inject
+	private PromotionService promotionService;
 	// @Scheduled(cron = "0 0 8 * * *")
 
 	@Scheduled(cron = "*/180 * * * * *")
@@ -38,17 +45,15 @@ public class ShedulerCronJob {
 
 					EmailMarketing emailMarketing = emailMarketingService
 							.getByIdLoaded(Long.valueOf(scheduler.getJob()));
-					if (emailMarketing.isActive()) {
+					if (emailMarketing.isActive() && emailMarketing.getDate().toString().equals(currentDateFormatted)) {
 						try {
 							result.setJobDate(new Date());
 							result.setScheduler(scheduler);
 							result.setMerchant(emailMarketing.getMerchant());
-							if (emailMarketing.getDate().toString().equals(currentDateFormatted)) {
-								emailMarketingService.sendMarketingEmail(emailMarketing);
-								result.setResult("<font color='green'>ok</font>");
-								jobResultService.save(result);
+							emailMarketingService.sendMarketingEmail(emailMarketing);
+							result.setResult("<font color='green'>ok</font>");
+							jobResultService.save(result);
 
-							}
 						} catch (Exception e) {
 							result.setResult("<font color='red'>ko</font>");
 							result.setDescription(e.getCause().getMessage().toString());
@@ -56,6 +61,35 @@ public class ShedulerCronJob {
 						}
 					}
 
+				} else if (scheduler.getType().equals(SchedulerType.PROMOTIONS.toString())) {
+					List<Promotion> promotionsStart = new ArrayList<Promotion>();
+					List<Promotion> promotionsStop = new ArrayList<Promotion>();
+					List<Promotion> promotions = promotionService.list();
+					for (Promotion promotion : promotions) {
+						if ((promotion.getPromotionType().equals(PromotionType.Category_Discount.toString())
+								|| promotion.getPromotionType().equals(PromotionType.Product_Discount.toString())
+								|| promotion.getPromotionType()
+										.equals(PromotionType.Manufacturer_Discount.toString())) && promotion.isActive() ) {
+							if(promotion.getStartDate().toString().equals(currentDateFormatted))
+							{
+								promotionsStart.add(promotion);
+								
+							}
+							else if(promotion.getEndDate().toString().equals(currentDateFormatted))
+							{
+								promotionsStop.add(promotion);
+							}
+
+						}
+						if(!promotionsStart.isEmpty())
+						{
+							promotionService.activatePromotions(promotionsStart);
+						}
+						if(!promotionsStop.isEmpty())
+						{
+							promotionService.disablePromotions(promotionsStop);
+						}
+					}
 				}
 			}
 		}
