668e87daf33a06f152a751656be84b6fc1bc99bb
Marcos <jmarcosjm@gmail.com>
2017-08-29 19:03:10 +0100

    remove unnecesary

--- src/main/java/com/salesmanager/shop/configuration/scheduler/ShedulerCronJob.java
+++ src/main/java/com/salesmanager/shop/configuration/scheduler/ShedulerCronJob.java
@@ -86,30 +86,7 @@ public class ShedulerCronJob {
 						}
 					}
 				} else if (scheduler.getType().equals(SchedulerType.ORDER_PROMOTIONS.toString())) {
-					List<Promotion> promotionsStart = new ArrayList<Promotion>();
-					List<Promotion> promotionsStop = new ArrayList<Promotion>();
-					List<Promotion> promotions = promotionService.list();
-					for (Promotion promotion : promotions) {
-						if ((promotion.getPromotionType().equals(PromotionType.Order_Discount.toString())
-								|| promotion.getPromotionType().equals(PromotionType.Free_Shipping.toString())
-								|| promotion.getPromotionType().equals(PromotionType.Threshold.toString())
-								|| promotion.getPromotionType().equals(PromotionType.Buy_X_get_Y_Free.toString()))
-								&& promotion.isActive()) {
-							if (promotion.getStartDate().toString().equals(currentDateFormatted)) {
-								promotionsStart.add(promotion);
-
-							} else if (promotion.getEndDate().toString().equals(currentDateFormatted)) {
-								promotionsStop.add(promotion);
-							}
-
-						}
-						if (!promotionsStart.isEmpty()) {
-							promotionService.activatePromotions(promotionsStart);
-						}
-						if (!promotionsStop.isEmpty()) {
-							promotionService.disablePromotions(promotionsStop);
-						}
-					}
+					
 				}
 			}
 		}
